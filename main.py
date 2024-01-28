5c17528363903458bd501714f2690181a2054646
DMGamage <gamagemadhushan@gmail.com>
2024-01-28 11:40:10 +0530

    fianl streamlit app

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ content2 = """
 
 st.info(content2)
 
-col3 , col4 = st.columns(2)
+col3 ,empty_col, col4 = st.columns([1.5 ,0.5 ,1.5])
 df = pd.read_csv("data.csv",sep=";")
 with col3 :
     for index,row in df[:10].iterrows():
